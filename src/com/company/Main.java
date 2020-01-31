4712a67d55dc4562647415598e8e47341b1d4c49
beknurbaihan <beknurbaihan@gmail.com>
2020-01-31 23:14:29 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        String[] names={"Nurjigit","Baiislam","Emirhan"};
+
+
+      switch (1){ case 1:
+          System.out.println("доброе утро , "+ names[0]);
+
+          case 2:
+              System.out.println("добрый день , "+ names[1]);
+
+          case 3:
+              System.out.println("добрый вечер , "+ names[2]);
+
+      }
+    }
+
+}
